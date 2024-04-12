8e6266391fd21c1fc2a5a54166e6ab38795f927c
npyott <nolan.pyott@gmail.com>
2024-04-12 02:50:57 -0400

    Add utilities for DynamoDB queries

--- server/aws.ts
+++ server/aws.ts
@@ -1,6 +1,12 @@
 import DynamoDB, { AttributeValue } from "aws-sdk/clients/dynamodb";
+import { Resource } from "ang-coupons-2023";
 
-import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } from "../scripting/config";
+import {
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_DYNAMODB_TABLE_NAME,
+} from "./config";
+import { ExtractTypedKeys, alphabetNumber, itMap } from "./util";
 
 export class ObjectTypeUnimplementedError extends Error {
     constructor(obj: any) {
@@ -14,15 +20,27 @@ export class AttributeTypeUnimplementedError extends Error {
     }
 }
 
+export class NotFoundError extends Error {
+    constructor(id: string) {
+        super(`Cannot find ${id}`);
+    }
+}
+
+export class AttributesNotReturnedError extends Error {
+    constructor() {
+        super("Attributes were not returned from an update query.");
+    }
+}
+
 export const dynamoDB = new DynamoDB({
     apiVersion: "2012-08-10",
     credentials: {
-        accessKeyId: AWS_ACCESS_KEY_ID,
-        secretAccessKey: AWS_SECRET_ACCESS_KEY,
+        accessKeyId: AWS_ACCESS_KEY_ID ?? "",
+        secretAccessKey: AWS_SECRET_ACCESS_KEY ?? "",
     },
 });
 
-export const jsObectToAttributeValue = (x: any): AttributeValue => {
+export const jsObjectToAttributeValue = (x: any): AttributeValue => {
     if (typeof x === "number" || typeof x === "bigint") {
         return { N: `${x}` };
     }
@@ -45,7 +63,7 @@ export const jsObectToAttributeValue = (x: any): AttributeValue => {
         }
 
         if (Array.isArray(x)) {
-            return { L: x.map(jsObectToAttributeValue) };
+            return { L: x.map(jsObjectToAttributeValue) };
         }
 
         if (x instanceof Date) {
@@ -63,7 +81,7 @@ export const jsObectToAttributeValue = (x: any): AttributeValue => {
                 return { NS: Array.from(x) };
             }
 
-            return { L: Array.from(x).map(jsObectToAttributeValue) };
+            return { L: Array.from(x).map(jsObjectToAttributeValue) };
         }
 
         const entries = Object.entries(x);
@@ -71,7 +89,7 @@ export const jsObectToAttributeValue = (x: any): AttributeValue => {
             M: Object.fromEntries(
                 entries.map(([key, value]) => [
                     key,
-                    jsObectToAttributeValue(value),
+                    jsObjectToAttributeValue(value),
                 ])
             ),
         };
@@ -80,7 +98,7 @@ export const jsObectToAttributeValue = (x: any): AttributeValue => {
     throw new ObjectTypeUnimplementedError(x);
 };
 
-export const attributeValueToJSObject = (x: AttributeValue) => {
+export const attributeValueToJSObject = (x: AttributeValue): any => {
     const unsupportedValueTypes: (keyof AttributeValue)[] = ["B", "BS"];
 
     for (const unsupportedValueType of unsupportedValueTypes) {
@@ -127,4 +145,110 @@ export const attributeValueToJSObject = (x: AttributeValue) => {
             ])
         );
     }
+};
+
+export const getItem = async <RT extends Resource>(id: RT["_id"]) => {
+    const res = await dynamoDB
+        .getItem({
+            Key: { _id: jsObjectToAttributeValue(id) },
+            TableName: AWS_DYNAMODB_TABLE_NAME ?? "",
+        })
+        .promise();
+
+    if (!res.Item) {
+        throw new NotFoundError(id);
+    }
+
+    return attributeValueToJSObject({ M: res.Item }) as RT;
+};
+
+export const putItem = async <RT extends Resource>(x: RT) => {
+    await dynamoDB
+        .putItem({
+            Item: jsObjectToAttributeValue(x).M!,
+            TableName: AWS_DYNAMODB_TABLE_NAME ?? "",
+        })
+        .promise();
+
+    return x;
+};
+
+export const updateItem = async <RT extends Resource>(
+    key: RT["_id"],
+    set: Partial<RT>,
+    inc: Partial<ExtractTypedKeys<RT, number>>,
+    append: Partial<ExtractTypedKeys<RT, any[]>>
+) => {
+    const attributes = new Set(
+        ...Object.keys(set),
+        ...Object.keys(inc),
+        ...Object.keys(append)
+    );
+
+    const attributeNameAlphaMap = new Map(
+        itMap(attributes, (attribute, index) => [
+            attribute,
+            alphabetNumber(index + 1),
+        ])
+    );
+
+    const res = await dynamoDB
+        .updateItem({
+            ExpressionAttributeNames: Object.fromEntries(
+                itMap(attributeNameAlphaMap, ([attribute, alpha]) => [
+                    `#${alpha}`,
+                    attribute,
+                ])
+            ),
+            ExpressionAttributeValues: Object.fromEntries([
+                ...Object.values(set).map((value, index) => [
+                    `:set${alphabetNumber(index + 1)}`,
+                    jsObjectToAttributeValue(value),
+                ]),
+                ...Object.values(inc).map((value, index) => [
+                    `:inc${alphabetNumber(index + 1)}`,
+                    jsObjectToAttributeValue(value),
+                ]),
+                ...Object.values(append).map((value, index) => [
+                    `:append${alphabetNumber(index + 1)}`,
+                    jsObjectToAttributeValue(value),
+                ]),
+            ]),
+            Key: {
+                _id: jsObjectToAttributeValue(key),
+            },
+            UpdateExpression: `SET ${[
+                ...Object.keys(set).map((attribute, index) => {
+                    const attributeAlpha =
+                        attributeNameAlphaMap.get(attribute)!;
+                    const expressionAlpha = `:set${alphabetNumber(index + 1)}`;
+
+                    return `${attributeAlpha} = ${expressionAlpha}`;
+                }),
+                ...Object.keys(inc).map((attribute, index) => {
+                    const attributeAlpha =
+                        attributeNameAlphaMap.get(attribute)!;
+                    const expressionAlpha = `:inc${alphabetNumber(index + 1)}`;
+
+                    return `${attributeAlpha} = ${attributeAlpha} + ${expressionAlpha}`;
+                }),
+                ...Object.keys(append).map((attribute, index) => {
+                    const attributeAlpha =
+                        attributeNameAlphaMap.get(attribute)!;
+                    const expressionAlpha = `:append${alphabetNumber(
+                        index + 1
+                    )}`;
+
+                    return `${attributeAlpha} = list_append(${attributeAlpha}, ${expressionAlpha})`;
+                }),
+            ].join(", ")}`,
+            TableName: AWS_DYNAMODB_TABLE_NAME ?? "",
+        })
+        .promise();
+
+    if (!res.Attributes) {
+        throw new AttributesNotReturnedError();
+    }
+
+    return attributeValueToJSObject({ M: res.Attributes }) as RT;
 };
--- server/util.ts
+++ server/util.ts
@@ -1,7 +1,50 @@
+import { CommonResource, Prefix } from "ang-coupons-2023";
 import { randomUUID } from "crypto";
 
-export const generateID = (prefix: string) => {
+export type ExtractTypedKeys<T, S> = {
+    [K in keyof T as T[K] extends S ? K : never]: T[K];
+};
+
+export const itMap = function* <T, R>(
+    it: Iterable<T>,
+    map: (value: T, index: number) => R
+) {
+    let index = 0;
+    for (const x of it) {
+        yield map(x, index);
+        ++index;
+    }
+};
+
+export const range = (start: number, end: number, increment = 1) => {
+    const array = new Array<number>();
+    for (let i = start; i < end; i += increment) {
+        array.push(i);
+    }
+
+    return array;
+};
+
+export const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+export const generateID = <P extends Prefix>(
+    prefix: P
+): CommonResource<P>["_id"] => {
     const uuid = randomUUID();
 
-    return `${prefix}-${uuid}`;
+    return `${prefix}+${uuid}`;
+};
+
+// From a positive integer, produces a string
+// A, B, C, ..., AA, AB, AC, ...
+export const alphabetNumber = (n: number): string => {
+    n = Math.max(Math.floor(n), 0);
+    if (n <= 0) {
+        return "";
+    }
+
+    const remainder = Math.floor(n % 26);
+    const quotient = Math.floor(n / 26);
+    const digit = alphabet[remainder];
+    return `${alphabetNumber(quotient)}${digit}`;
 };
