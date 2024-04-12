012d3edc7f1cf83f8f6aa41413cac0e078a4d2d1
npyott <nolan.pyott@gmail.com>
2024-04-12 02:49:50 -0400

    Update config - Include dynamoDB table (single table design) - Duplicate to sever (TODO: merge and use Docker)

--- scripting/config.ts
+++ scripting/config.ts
@@ -1,24 +1,27 @@
 import { readFileSync } from "fs";
 
-const data = readFileSync(".env").toString();
+try {
+    const data = readFileSync(".env").toString();
 
-const keyValuePairs = data
-    .split("\n")
-    .filter((line) => line.includes("="))
-    .map((line) => {
-        const [key, value] = line
-            .split("=")
-            .map((s) => s.trim())
-            .map((s) =>
-                s.startsWith('"') && s.endsWith('"') ? s.slice(1, -1) : s
-            );
+    const keyValuePairs = data
+        .split("\n")
+        .filter((line) => line.includes("="))
+        .map((line) => {
+            const [key, value] = line
+                .split("=")
+                .map((s) => s.trim())
+                .map((s) =>
+                    s.startsWith('"') && s.endsWith('"') ? s.slice(1, -1) : s
+                );
 
-        return [key, value];
-    });
+            return [key, value];
+        });
 
-for (const [key, value] of keyValuePairs) {
-    process.env[key] = value;
-}
+    for (const [key, value] of keyValuePairs) {
+        process.env[key] = value;
+    }
+} catch (_) {}
 
 export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+export const AWS_DYNAMODB_TABLE_NAME = process.env.AWS_DYNAMODB_TABLE_NAME;
---
+++ server/config.ts
@@ -0,0 +1,3 @@
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+export const AWS_DYNAMODB_TABLE_NAME = process.env.AWS_DYNAMODB_TABLE_NAME;
