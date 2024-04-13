d8a2c71fbf97623164902184a19f40ea0563d374
npyott <nolan.pyott@gmail.com>
2024-04-13 17:02:38 -0400

    WIP - Remove autogen codefrom app - Add more dependencies to dashbaord

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div> Hello world! </div>,
+    },
+    {
+        path: "/test",
+        element: <div> test! </div>,
+    },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [authToken, setAuthToken] = useState<string>();
+
+    useEffect(() => {
+        const localAuthToken = localStorage.getItem("authToken");
+
+        if (localAuthToken) {
+            setAuthToken(localAuthToken);
+        }
+    }, []);
+
+    return <RouterProvider router={router} />;
 }
 
 export default App;
